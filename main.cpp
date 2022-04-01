591af02f9449c1bd18e725909d59441fbfb704a1
AliceFavalessa <101601701+AliceFavalessa@users.noreply.github.com>
2022-04-01 10:28:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+   float a=0, b=0, x=0, y=0;
+  cin >> a >> b >> x >> y;
+    if (x<0 and y>0){
+       cout << a*x - b*y;
+    }else if(x>=0 and y<=0){
+        cout << a*(x*x) - b*y;
+    }else{
+        cout << a*x + b*(y*y);
+    }
    return 0;
 }
